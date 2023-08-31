b71154ae42cd71b122fdad1a8c8159117cbcc7d7
John Sanpe <sanpeqf@gmail.com>
2023-08-31 11:43:24 +0800

    feat main: increase operating frequency
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- src/main.c
+++ src/main.c
@@ -2,7 +2,23 @@
 
 int main(void)
 {
+    RCC_OscInitTypeDef OscInitType;
+    RCC_ClkInitTypeDef ClkInitType;
     GPIO_InitTypeDef GPIOInitType;
+
+    OscInitType.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    OscInitType.HSICalibrationValue = RCC_HSICALIBRATION_24MHz;
+    OscInitType.HSIState = RCC_HSI_ON;
+    OscInitType.HSIDiv = RCC_HSI_DIV1;
+    OscInitType.PLL.PLLState = RCC_PLL_ON;
+    OscInitType.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    HAL_RCC_OscConfig(&OscInitType);
+
+    ClkInitType.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1;
+    ClkInitType.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    ClkInitType.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    ClkInitType.APB1CLKDivider = RCC_HCLK_DIV1;
+    HAL_RCC_ClockConfig(&ClkInitType, FLASH_LATENCY_1);
 
     HAL_Init();
     __HAL_RCC_GPIOB_CLK_ENABLE();
