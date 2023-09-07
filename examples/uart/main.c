108e453704a4d863101ba18719a3d413ca2e25aa
John Sanpe <sanpeqf@gmail.com>
2023-09-07 11:07:53 +0800

    feat examples: added uart printf demo
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ examples/uart/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "py32f0xx_hal.h"
+
+UART_HandleTypeDef huart1;
+
+int __io_putchar(int ch)
+{
+    if (ch == '\n')
+        HAL_UART_Transmit(&huart1, (void *)"\r", 1, -1);
+    HAL_UART_Transmit(&huart1, (void *)&ch, 1, -1);
+    return ch;
+}
+
+int main(void)
+{
+    RCC_OscInitTypeDef OscInitType = {};
+    RCC_ClkInitTypeDef ClkInitType = {};
+    GPIO_InitTypeDef GPIOInitType = {};
+
+    HAL_Init();
+
+    OscInitType.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    OscInitType.HSICalibrationValue = RCC_HSICALIBRATION_24MHz;
+    OscInitType.HSIState = RCC_HSI_ON;
+    OscInitType.HSIDiv = RCC_HSI_DIV1;
+    OscInitType.PLL.PLLState = RCC_PLL_ON;
+    OscInitType.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    HAL_RCC_OscConfig(&OscInitType);
+
+    ClkInitType.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1;
+    ClkInitType.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    ClkInitType.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    ClkInitType.APB1CLKDivider = RCC_HCLK_DIV1;
+    HAL_RCC_ClockConfig(&ClkInitType, FLASH_LATENCY_1);
+
+    GPIOInitType.Pin = GPIO_PIN_2 | GPIO_PIN_3;
+    GPIOInitType.Mode = GPIO_MODE_AF_PP;
+    GPIOInitType.Pull = GPIO_PULLUP;
+    GPIOInitType.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIOInitType.Alternate = GPIO_AF1_USART1;
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    HAL_GPIO_Init(GPIOA, &GPIOInitType);
+
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 115200;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    __HAL_RCC_USART1_CLK_ENABLE();
+    HAL_UART_Init(&huart1);
+
+    for (;;) {
+        printf("helloworld\n");
+        HAL_Delay(500);
+    }
+
+    return 0;
+}
